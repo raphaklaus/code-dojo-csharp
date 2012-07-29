1df8c78519374877f3908b6aea0d25a1e81f8af3
Raphael M. Duarte Pinheiro <sonata_secret@hotmail.com>
2012-07-29 00:19:56 -0300

    Diamond Challenge

---
+++ Diamond/Diamond.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Text;
+
+
+namespace CodeDojoDiamond
+{
+    class Diamond
+    {
+        String[] Alphabet = 
+        {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z"
+        };
+
+        public string Process(string param1)
+        {
+            int max;
+            max = Array.IndexOf(Alphabet, param1);
+            
+
+            StringBuilder txt = new StringBuilder();
+            int countOuterSpaces = max;
+            int countInnerSpaces = -1;
+
+                for (int i = 0; i < max; i++)
+                {
+                    
+                    
+                    txt.Append(doSpace(countOuterSpaces, " "));
+
+                    if (i == 0)
+                        txt.Append(Alphabet[i]);
+                    else
+                    {
+                        txt.Append(Alphabet[i]);
+                        txt.Append(doSpace(countInnerSpaces, " "));
+                        txt.Append(Alphabet[i]);                
+                    }
+                
+                    txt.AppendLine();
+                    countOuterSpaces -= 1;
+                    countInnerSpaces += 2;
+                }
+
+
+                for (int j = max; j >= 0; j--)
+                {
+                    txt.Append(doSpace(countOuterSpaces, " "));
+
+                    if (j == 0)
+                        txt.Append(Alphabet[j]);
+                    else
+                    {
+                        txt.Append(Alphabet[j]);
+                        txt.Append(doSpace(countInnerSpaces, " "));
+                        txt.Append(Alphabet[j]);
+                    }
+
+
+                    txt.AppendLine();
+                    countOuterSpaces += 1;
+                    countInnerSpaces -= 2;
+                    
+                }
+           
+            return txt.ToString();
+        }
+
+        public string doSpace(int param1, string param2)
+        {
+            string sEspaco = "";
+            
+            for (int i = 0; i < param1; i++)
+            {
+                sEspaco += param2;    
+            }
+
+            return sEspaco;
+
+        }
+
+    }
+}
